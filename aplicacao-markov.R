f8f14bde9e9f5cc20f4f4af2a28d4e542d82d392
Allisson Santos <allissonst@hotmail.com>
2022-03-12 23:18:15 -0300

    adicionando código para novos gráficos

--- aplicacao-markov.R
+++ aplicacao-markov.R
@@ -8,7 +8,7 @@ options(scipen = 9999)
 
 IIE=dados$IIE
 
-#regress„o linear
+#regress√£o linear
 mod<-lm(IIE ~ 1)
 
 #aplicando markov para encontrar os regimes.
@@ -17,9 +17,14 @@ m1=msmFit(mod,k=2, sw=c(T,T))
 #Mostrando os resultados
 print(m1)
 
-#plotando os resÌduos
+#plotando os res√≠duos
 plot(m1)
 
 #plotando as probabilidades para cada regime
 plotProb(m1,1)
 
+par(mar=c(0.1,0.1,0.1,0.1))
+plotProb(m1,2)
+
+par(mar=c(0.1,0.1,0.1,0.1))
+plotProb(m1,3)
